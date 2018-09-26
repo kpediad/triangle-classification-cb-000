553ef2e62fd832d8efdc4b2cf0719c77757a20d3
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-26 14:44:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if @side1 != 0 && @side2 != 0 && @side3 != 0 && @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1
+      if @side1 == @side2 || @side1 == @side3
+        if @side2 == @side3
+          :equilateral
+        else
+          :isosceles
+        end
+      end
+      :scalene
+    else
+      #raiseerror and rescue
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle sides are invalid!"
+    end
+  end
 end
